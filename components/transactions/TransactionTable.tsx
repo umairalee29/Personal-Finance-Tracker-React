d450233dec164c3e62db1dc380fa2427bf45aec8
umairalee29 <umair.alee93@gmail.com>
2026-05-28 15:56:59 +0200

    Change the icon of edit and delete to SVG

--- components/transactions/TransactionTable.tsx
+++ components/transactions/TransactionTable.tsx
@@ -150,17 +150,21 @@ export function TransactionTable({ transactions, isLoading, onRefetch, currency 
                     <div className="flex items-center gap-1">
                       <button
                         onClick={() => setEditTarget(t)}
-                        className="btn-ghost !p-1.5 text-xs"
                         title="Edit"
+                        className="p-1.5 rounded-lg text-slate-400 hover:text-slate-700 dark:hover:text-slate-200 hover:bg-slate-100 dark:hover:bg-slate-700 transition-colors"
                       >
-                        ✏️
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" viewBox="0 0 20 20" fill="currentColor">
+                          <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                        </svg>
                       </button>
                       <button
                         onClick={() => setDeleteTarget(t)}
-                        className="btn-ghost !p-1.5 text-xs text-rose-500 hover:text-rose-700"
                         title="Delete"
+                        className="p-1.5 rounded-lg text-slate-400 hover:text-rose-600 dark:hover:text-rose-400 hover:bg-rose-50 dark:hover:bg-rose-950/40 transition-colors"
                       >
-                        🗑
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" viewBox="0 0 20 20" fill="currentColor">
+                          <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
+                        </svg>
                       </button>
                     </div>
                   </td>
